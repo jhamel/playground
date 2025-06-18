84c4c792708a1f6d5ad1836cf1efbced4d94f7ab
Johannes <johannes@jhamel.dev>
2025-06-18 17:39:02 +0200

    init

--- src/main/java/dev/jhamel/Say.java
+++ src/main/java/dev/jhamel/Say.java
@@ -1,8 +1,9 @@
 package dev.jhamel;
 
 public class Say {
+    public static final String HELLO_WORLD = "Hello World!";
 
     public String say() {
-        return "Hello World!";
+        return HELLO_WORLD;
     }
 }
