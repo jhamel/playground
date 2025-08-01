c2837caed26824f537916bfff065cfc779bf1a47
Johannes <johannes@jhamel.dev>
2025-08-01 15:45:46 +0200

    init

--- src/test/java/dev/jhamel/HelloWorldTest.java
+++ src/test/java/dev/jhamel/HelloWorldTest.java
@@ -1,8 +1,9 @@
 package dev.jhamel;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloWorldTest {
 
