c75e99dc6509bc31114c0c54f0046001d2d49ee8
Johannes <johannes@jhamel.dev>
2025-08-05 13:10:57 +0200

    init

---
+++ src/test/java/dev/jhamel/HelloWorld2Test.java
@@ -0,0 +1,14 @@
+package dev.jhamel;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorld2Test {
+
+    @Test
+    public void testHelloWorld() {
+        assertEquals(2,1);
+    }
+}
